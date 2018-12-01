b6e7ba836a7b0274a698e1f6b8970788255f1f1d
s-livingstone <44588061+s-livingstone@users.noreply.github.com>
2018-12-01 11:41:14 -0500

    Update othercodeoption.java

--- othercodeoption.java
+++ othercodeoption.java
@@ -4,9 +4,12 @@ public class Adventure {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        String firstOption, secondOption, thirdOption;
+        String firstOption, secondOption, thirdOption, major;
         System.out.println("WELCOME TO THE BRANDEISIAN EXPERIENCE!");
         System.out.println();
+	System.out.println("What is your field of study? stem or humanities");
+	major = keyboard.next();
+	    
         System.out.println("Good Morning! It's a Monday and you need to get ready for class. What do you eat for breakfast? \"yougurt in the dorm room\" or into the");
         System.out.println("\"Einsteins\"\"Lower\" ?");
         System.out.print("> ");
@@ -17,6 +20,20 @@ public class Adventure {
             
 		if (firstOption.equalsIgnoreCase("Yogurt in the dorm room"))
             		System.out.println("Yum but don't be late/go back into bed")
-		if(firstOption.equalsIgnoreCase("Esteins"))
-				System.out.println("Yum!")
+		if(firstOption.equalsIgnoreCase("Ensteins"))
+				System.out.println("Yum!");
+	System.out.println("whoops! on your way to class you bumped into someone and their drink spilled all over them! what do you do?");
+			if major = stem
+				System.out.println("im premed, i dont have time for this. out my way im late for my 8am [1]")
+				System.out.println("Help them because you are a true brandeis student [2]")
+				
+				else
+				System.out.println("keep going you are late [1]")
+				System.out.println("Help them because you are a true brandeis student [2]")
+					
+				
+
+				
+			
+
 			
